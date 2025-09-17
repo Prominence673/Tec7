da2d87129a9a16ff1f0345d295df5903e5053567
Hi-katu <noway@gmail.com>
2025-09-17 15:07:24 -0300

    Navbar primer diseño

---
+++ src/pages/Home.jsx
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <div className="home-container">
+      <h1>Bienvenido a la página de inicio</h1>
+      <p>Esta es la página principal de la aplicación.</p>
+    </div>
+  );
+}
